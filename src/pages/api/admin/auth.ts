c6023504b45a80bfb3715965cb0184e24171d771
calebtuttle <1calebtuttle@gmail.com>
2023-01-16 19:29:46 -0500

    modify log

--- src/pages/api/admin/auth.ts
+++ src/pages/api/admin/auth.ts
@@ -12,7 +12,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<any>) 
   }
 
   if (apiKey != process.env.ADMIN_API_KEY) {
-    console.log(process.env.ADMIN_API_KEY);
+    console.log("admin", process.env.ADMIN_API_KEY);
     console.log("GET /admin/auth: API key not valid");
     return res.status(401).json({ error: "API key not valid" });
   }
