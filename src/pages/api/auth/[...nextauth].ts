69043597dd689869b582d8cb7ae83eb14368e3a0
calebtuttle <1calebtuttle@gmail.com>
2023-01-19 09:05:50 -0500

    Update credentials provider name

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -8,7 +8,7 @@ export const authOptions = {
   providers: [
     CredentialsProvider({
       // The name to display on the sign in form (e.g. "Sign in with...")
-      name: "Holonym Client Portal",
+      name: "Username and Password",
       // `credentials` is used to generate a form on the sign in page.
       // You can specify which fields should be submitted, by adding keys to the `credentials` object.
       // e.g. domain, username, password, 2FA token, etc.
